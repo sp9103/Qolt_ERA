354b0c1f98592682124982a6ce63f57db05cd23d
naubull2 <naubull2@gmail.com>
2014-01-23 23:49:34 +0900

    * LibERA is a library project for JNI calls (currently added YUV converter function only) - ndk-build is already done, ready for testing

--- colorblind/gen/com/naubull2/colorblind/R.java
+++ colorblind/gen/com/naubull2/colorblind/R.java
@@ -42,16 +42,16 @@ containing a value of this type.
         public static final int splash=0x7f020002;
     }
     public static final class id {
-        public static final int any=0x7f040000;
-        public static final int back=0x7f040001;
-        public static final int front=0x7f040002;
-        public static final int imageView=0x7f040009;
-        public static final int main_button_camera=0x7f040006;
-        public static final int main_button_demo=0x7f040004;
-        public static final int main_button_image=0x7f040005;
-        public static final int main_button_movie=0x7f040007;
-        public static final int main_button_setting=0x7f040008;
-        public static final int main_button_test=0x7f040003;
+        public static final int any=0x7f050000;
+        public static final int back=0x7f050001;
+        public static final int front=0x7f050002;
+        public static final int imageView=0x7f050009;
+        public static final int main_button_camera=0x7f050006;
+        public static final int main_button_demo=0x7f050004;
+        public static final int main_button_image=0x7f050005;
+        public static final int main_button_movie=0x7f050007;
+        public static final int main_button_setting=0x7f050008;
+        public static final int main_button_test=0x7f050003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,7 +59,7 @@ containing a value of this type.
         public static final int fragment_full_image=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
