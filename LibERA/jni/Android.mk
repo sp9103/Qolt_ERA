07e6c5f0429b88e5f8cd19178b9ccb758d8e8aad
sp9103 <sp9103@naver.com>
2014-01-24 00:33:09 +0900

    *새 형식 맞춤 commit

--- LibERA/jni/Android.mk
+++ LibERA/jni/Android.mk
@@ -2,6 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+OPENCV_CAMERA_MODULE:=on
+OPENCV_INSTALL_MODULES:=on
+include E:\Document\workspace\OpenCV-2.4.8-android-sdk\OpenCV-2.4.8-android-sdk\sdk\native\jni\OpenCV.mk
+
 LOCAL_MODULE    := ERA
 LOCAL_SRC_FILES := ERAcore.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
