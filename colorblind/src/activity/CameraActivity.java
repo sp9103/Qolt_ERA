4f0dfdca73c4a7bf30049dab09992971f6263fc6
naubull2 <naubull2@gmail.com>
2014-01-25 00:44:03 +0900

    Start implementing camera module (appropriately oriented)

---
+++ colorblind/src/activity/CameraActivity.java
@@ -0,0 +1,61 @@
+package activity;
+
+import android.app.Activity;
+
+public class CameraActivity extends Activity{
+	
+	/*
+	 * 
+	 * bunch of member objects
+	 * 
+	 */
+
+	/*
+	 * Main handler for posting messages to main UI thread
+	 */
+	
+	/*
+	 * Initializer method
+	 */
+	
+	/*
+	 * orientation listener
+	 */
+	
+	
+	/*
+	 * shutter callback
+	 */
+	
+	/*
+	 * picture callback
+	 */
+	
+	/*
+	 * focus callback
+	 */
+	
+	/*
+	 * get camera instance
+	 */
+	
+	/*
+	 * release camera instance
+	 */
+	
+	/*
+	 * onCreate()
+	 */
+	
+	/*
+	 * onResume()
+	 */
+	
+	/*
+	 * onPause()
+	 */
+	
+	/*
+	 * onDestroy()
+	 */
+}
