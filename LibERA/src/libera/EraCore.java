e4f5a65c4f43a1c5fc390b591a9e709e137a1897
sp9103 <sp9103@naver.com>
2014-01-27 14:56:12 +0900

    * master branch merge 직전 커밋 * JNI 인터페이스 구성 완료 - 테스팅 진행 필요

--- LibERA/src/libera/EraCore.java
+++ LibERA/src/libera/EraCore.java
@@ -25,4 +25,22 @@ public class EraCore {
 	 */
 	public native void decodeYUV420SP(byte[] yuv420sp, int width, int height, int[] rgbOut);
 	
+	///////////////////////////////////////////////////Image Process ERA Func//////////////////////////////////////////////////////////////////////
+	
+	/*Open Data File, call this func -> real time convert start (one time)
+	  - file open -> copy buffer -> file close*/
+	public native void OpenDataFile(String FilePath);
+	/*Delete Data_Matrix. call this func => real time convert exit*/
+	public native int DeleteDataBuffer();
+	/*image color weakness correction - Static Image*/
+	public native void RefineImage(long src, long dst, float factor);
+	/*image color weakness experience - Static Image*/
+	public native void InverseImage(long src, long dst, float factor);
+	/*Tree file Craete*/
+	public native boolean MakeTreeFile(int interval, float factor, String FilePath, int mode);
+	/*Using Tree, Make Image*/
+	public native int MakeImgtoData(long src, long dst);
+
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
