ec8a76fe49509fb0d6f5c1b792255ee3507d6ece
naubull2 <naubull2@gmail.com>
2014-02-02 17:48:36 +0900

    Added setting activity, with preference xml

---
+++ colorblind/src/activity/SettingActivity.java
@@ -0,0 +1,15 @@
+package activity;
+
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+
+public class SettingActivity extends PreferenceActivity implements OnPreferenceClickListener{
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
